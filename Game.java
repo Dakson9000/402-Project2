bafb1565ff11ea6dca7e62f705de9ee181075398
Dakson9000 <masonmiles@outlook.com>
2022-11-17 17:10:01 -0800

    user input, instanciate worldstate and planner

--- Game.java
+++ Game.java
@@ -1,6 +1,41 @@
+import java.util.*;
 public class Game {
-  
+  Scanner scan = new Scanner(System.in);
   public void startGame() {
-    
+    line();
+    String monkeyRoom = getRoom("Monkey");
+    String boxRoom = getRoom("Box");
+    String bananaRoom = getRoom("Banana");
+  }
+
+  public void line() {
+    System.out.println("====================================================");
+  }
+
+  public String getRoom(String object) {
+    boolean gotValidRoom = false;
+    String userRoom = "";
+    while (!gotValidRoom) {
+      System.out.println("Select which room the " + object + " starts in:");
+      System.out.println("[1] Room A");
+      System.out.println("[2] Room B");
+      System.out.println("[3] Room C");
+      System.out.print("==> ");
+
+      userRoom = scan.nextLine();
+      System.out.println();
+
+      gotValidRoom = isRoom(userRoom);
+      if (!gotValidRoom) {
+        System.out.println("Sorry, not valid room\n");
+      }
+    }
+    return userRoom;
+  }
+
+  public boolean isRoom(String tryRoom) {
+    return (tryRoom.equalsIgnoreCase("A") 
+    || tryRoom.equalsIgnoreCase("B") 
+    || tryRoom.equalsIgnoreCase("C"));
   }
 }
--- WorldState.java
+++ WorldState.java
@@ -1,18 +1,26 @@
 public class WorldState {
-  private String roomMonkeyIn;
-  private String roomBoxIn;
-  private String roomBananasIn;
+  private String monkeyRoom;
+  private String boxRoom;
+  private String bananaRoom;
   private boolean monkeyLow;
   private boolean monkeyHasBananas;
 
-  public static final String ROOM_A = "A";
+  public static final String A = "A";
+  public static final String B = "B";
+  public static final String C = "C";
 
-  public String getRoomMonkeyIn(){
-      return this.roomMonkeyIn;
+  public WorldState(String monkey, String box, String banana) {
+    monkeyRoom = monkey;
+    boxRoom = box;
+    bananaRoom = banana;
+  }
+  public String getMonkeyRoom() {
+    return bananaRoom;
+    
   }
 
   public boolean isMonkeyAt(String room) {
-      return this.roomMonkeyIn.equalsIgnoreCase(room);
+    return this.roomMonkeyIn.equalsIgnoreCase(room);
   }
 
   public boolean isLow() {
